88590773835db88884f417ac9bcc9be21e82d651
Cyril Scerbin <kshcherb@umich.edu>
2026-02-28 05:41:31 -0800

    [EDA] Initial Queries (#2)
    * add exploration queries
    
    * fix: add missing newline and limit to SQL query for GitHub repositories with Python

---
+++ sql/github_repos_with_python_lang.sql
@@ -0,0 +1,14 @@
+/*
+This query retrieves the names of GitHub repositories that have Python as one of their programming languages. 
+The query checks for the presence of 'Python' in the list of languages for each repository and returns the corresponding repository names.
+*/
+
+SELECT
+  repo_name
+FROM
+  `bigquery-public-data.github_repos.languages` AS l
+WHERE EXISTS (
+    SELECT 1 FROM UNNEST(l.language) AS lang WHERE lang.name = 'Python'
+)
+LIMIT 100
+;
