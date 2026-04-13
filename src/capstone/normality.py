70a856967cc9f6fde4c1ce9ab9c43cc655c46aee
Cyril Scerbin <kshcherb@umich.edu>
2026-04-13 04:48:42 +0000

    Add pingouin dependency and implement distribution plotting function
    - Added "pingouin>=0.6.1" to the project dependencies in pyproject.toml and uv.lock.
    - Created a new module `normality.py` with a function `plot_distributions` to visualize distributions of specified columns in a DataFrame using seaborn.

---
+++ src/capstone/normality.py
@@ -0,0 +1,23 @@
+import math
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def plot_distributions(data: pd.DataFrame, columns: list[str]) -> None:
+    ncols = math.ceil(len(columns) ** 0.5)
+    nrows = math.ceil(len(columns) / ncols)
+
+    fig, axes = plt.subplots(nrows, ncols, figsize=(10, 8))
+    axes = axes.flatten()
+
+    for i, col in enumerate(columns):
+        sns.histplot(data[col].dropna(), kde=True, ax=axes[i])
+        axes[i].set_title(col)
+
+    # remove empty plots
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plt.tight_layout()
+    plt.show()
